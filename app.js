d79eded0a560c26258b29207067e62a36ac92322
wenipsptsr <wenipuspitasari029@gmail.com>
2024-10-24 13:47:34 +0700

    selesai

--- app.js
+++ app.js
@@ -9,7 +9,19 @@
  */
 
 // TULIS SCRIPT KAMU DI SINI
+function calculateKomisi(nilaiKontrak) {
+  if (nilaiKontrak >= 5000000) {
+    return nilaiKontrak * 0.05;
+  }
+  else if (nilaiKontrak >= 1000000) {
+    return 100000;
+  }
+  else {
+    return 0;
+  }
+}
 
+console.log(calculateKomisi(100000))
 
 
 // JANGAN HAPUS / UBAH SCRIPT DI BAWAH
